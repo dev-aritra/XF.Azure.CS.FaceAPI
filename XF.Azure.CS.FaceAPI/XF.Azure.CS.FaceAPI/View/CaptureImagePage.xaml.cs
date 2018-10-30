a5c699bc86318d14f7ada07f0eccfe1ef159fb6a
Aritra Das <adas@vsoftconsulting.com>
2018-10-30 15:24:55 +0530

    Getting response from face api

--- XF.Azure.CS.FaceAPI/XF.Azure.CS.FaceAPI/View/CaptureImagePage.xaml.cs
+++ XF.Azure.CS.FaceAPI/XF.Azure.CS.FaceAPI/View/CaptureImagePage.xaml.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.Azure.CognitiveServices.Vision.Face;
+using Microsoft.Azure.CognitiveServices.Vision.Face.Models;
 using Plugin.Media;
+using Plugin.Media.Abstractions;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
+using System.Reflection;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
@@ -12,69 +14,129 @@ using Xamarin.Forms.Xaml;
 
 namespace XF.Azure.CS.FaceAPI.View
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class CaptureImagePage : ContentPage
-	{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CaptureImagePage : ContentPage
+    {
         private ApiKeyServiceClientCredentials credentials;
         private FaceClient faceClient;
         private FaceOperations faceOperations;
-		public CaptureImagePage ()
-		{
-			InitializeComponent ();
-            InitializeFaceClient();
-            var capturedImage = InitCamera();
-            
 
-		}
-        private async Task<Stream> InitCamera()
+        private Stream capturedImageStream;
+        public CaptureImagePage()
         {
+            InitializeComponent();
+            InitOperation();
+
+
+        }
+
+        private async void InitOperation()
+        {
+            InitializeFaceClient();
+            var capturedImage = await InitCamera();
+            if (capturedImage != null)
+            {
+                var emotions = await GetFaces(capturedImage);
+                var detectedEmotion = FindDetectedEmotion(emotions);
+            }
+        }
+
+        private string FindDetectedEmotion(List<Emotion> emotions)
+        {
+            float max = 0;
+            PropertyInfo prop = null;
+            try
+            {
+                var emotionsValues = typeof(Emotion).GetProperties();
+                foreach (var property in emotionsValues)
+                {
+                    if ((double)property.GetValue(emotions[0]) > max)
+                    {
+                        max = (float)property.GetValue(emotions[0]);
+                        prop = property;
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+
+            }
+            
+            return prop.PropertyType.ToString();
+        }
+
+        private async Task<MediaFile> InitCamera()
+        {
+            MediaFile image = null;
             await CrossMedia.Current.Initialize();
             if (CrossMedia.Current.IsCameraAvailable && CrossMedia.Current.IsTakePhotoSupported)
             {
-                var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+
+                image = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
                 {
                     RotateImage = false,
                     DefaultCamera = Plugin.Media.Abstractions.CameraDevice.Front,
                     Directory = "FaceAPI",
                     Name = "face.jpg"
                 });
-
-                if (file == null)
-                    return null;
-
-                capturedImage.Source = ImageSource.FromStream(() =>
+                if (image != null)
                 {
-                    var stream = file.GetStream();
-                    return stream;
-                });
-                return file.GetStream();
-                
-                
+                    //capturedImage.Source = ImageSource.FromStream(() =>
+                    //{
+                    //    var stream = file.GetStream();
+                    //    imageStream = stream;
+                    //    return stream;
+                    //});
+                    
+                    capturedImage.Source = ImageSource.FromStream(() => {
+                        return image.GetStream();
+                    });
+                }
+                else
+                {
+                    await DisplayAlert("No Camera", ":( No camera available.", "OK");
+                    
+                }
+
+
+
 
             }
             else
             {
-                await DisplayAlert("No Camera", ":( No camera available.", "OK");
-                return null;
+                
+
             }
+            return image;
 
-            
         }
 
-        private async Task<dynamic> GetFaces(Stream image)
+        private async Task<List<Emotion>> GetFaces(MediaFile image)
         {
-            var faces = faceOperations.DetectWithStreamAsync(image);
-           
-            
-
-            return null;
+            List<Emotion> faces = null;
+            try
+            {
+                
+                var faceApiResponseList = await faceClient.Face.DetectWithStreamAsync(image.GetStream(), returnFaceAttributes: new List<FaceAttributeType> { { FaceAttributeType.Emotion } });
+                faces = faceApiResponseList.Select(x => x.FaceAttributes.Emotion).ToList();
+                
+            }
+            catch(Exception ex)
+            {
+                
+            }
+            return faces;
         }
+
+
+
+
 
         private void InitializeFaceClient()
         {
             credentials = new ApiKeyServiceClientCredentials("50c78f754e734425adca685ee646701b");
             faceClient = new FaceClient(credentials);
-            faceClient.Endpoint = "https://centralindia.api.cognitive.microsoft.com/face/v1.0";
+            faceClient.Endpoint = "https://centralindia.api.cognitive.microsoft.com/";
             faceOperations = new FaceOperations(faceClient);
         }
     }
