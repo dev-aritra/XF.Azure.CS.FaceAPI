1d4dc7c0431c10296537bd5d9eea99a1ae1a779b
Aritra Das <adas@vsoftconsulting.com>
2018-11-06 18:42:58 +0530

    All functionalities completed

--- XF.Azure.CS.FaceAPI/XF.Azure.CS.FaceAPI/View/CaptureImagePage.xaml.cs
+++ XF.Azure.CS.FaceAPI/XF.Azure.CS.FaceAPI/View/CaptureImagePage.xaml.cs
@@ -6,7 +6,6 @@ using Plugin.Media.Abstractions;
 using SkiaSharp;
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
@@ -22,9 +21,8 @@ namespace XF.Azure.CS.FaceAPI.View
         private ApiKeyServiceClientCredentials credentials;
         private FaceClient faceClient;
         private FaceOperations faceOperations;
-        private string lastResult;
 
-        private List<FaceRectangle> faceRectangles;
+        private List<DetectedFaceExtended> detectedFaces;
         private SKBitmap Image;
 
         public CaptureImagePage()
@@ -35,7 +33,7 @@ namespace XF.Azure.CS.FaceAPI.View
 
         private async void InitOperation()
         {
-            faceRectangles= new List<FaceRectangle>();
+            detectedFaces = new List<DetectedFaceExtended>();
             InitializeFaceClient();
             await CrossMedia.Current.Initialize();
             TakePictureAndAnalizeImage();
@@ -43,9 +41,9 @@ namespace XF.Azure.CS.FaceAPI.View
 
 
 
-        private string FindDetectedEmotion(List<Emotion> emotions)
+        private string FindDetectedEmotion(Emotion emotion)
         {
-            lastResult = string.Empty;
+            
             double max = 0;
             PropertyInfo prop = null;
             try
@@ -53,34 +51,34 @@ namespace XF.Azure.CS.FaceAPI.View
                 var emotionsValues = typeof(Emotion).GetProperties();
                 foreach (var property in emotionsValues)
                 {
+
+                    var val = (double)property.GetValue(emotion);
                     
-                    var val = (double)property.GetValue(emotions[0]);
-                    lastResult += property.Name + ": " + val + "\n";
-                    if ( val > max)
+                    if (val > max)
                     {
                         max = val;
                         prop = property;
                     }
                 }
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
 
             }
-            
+
             return prop.Name.ToString();
         }
 
         private async Task<MediaFile> TakePicture()
         {
             Image = null;
-            MediaFile image = null;     
+            MediaFile image = null;
             if (CrossMedia.Current.IsCameraAvailable && CrossMedia.Current.IsTakePhotoSupported)
             {
 
                 image = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
                 {
-                    PhotoSize=PhotoSize.Medium,
+                    PhotoSize = PhotoSize.Medium,
                     RotateImage = true,
                     DefaultCamera = Plugin.Media.Abstractions.CameraDevice.Front,
                     Directory = "FaceAPI",
@@ -97,33 +95,46 @@ namespace XF.Azure.CS.FaceAPI.View
 
         private void SetImageInImageView(MediaFile image)
         {
-            Image= SKBitmap.Decode(image.GetStreamWithImageRotatedForExternalStorage());
+            Image = SKBitmap.Decode(image.GetStreamWithImageRotatedForExternalStorage());
             capturedImage.InvalidateSurface();
         }
 
-        private async Task<List<Emotion>> GetFaces(MediaFile image)
+        private async Task<bool> GetFaces(MediaFile image)
         {
-            List<Emotion> faces = null;
+            bool facesFound = false;
             try
             {
-                
+
                 var faceApiResponseList = await faceClient.Face.DetectWithStreamAsync(image.GetStream(), returnFaceAttributes: new List<FaceAttributeType> { { FaceAttributeType.Emotion } });
-                faces = faceApiResponseList.Select(x => x.FaceAttributes.Emotion).ToList();
-                if(faceRectangles.Count>0)
+                if (detectedFaces.Count > 0)
                 {
-                    faceRectangles.Clear();
+                    detectedFaces.Clear();
                 }
-                foreach(var face in faceApiResponseList)
+
+                DetectedFaceExtended decFace = null;
+                if(faceApiResponseList.Count>0)
                 {
-                    faceRectangles.Add(face.FaceRectangle);
+                    facesFound = true;
+                    foreach (var face in faceApiResponseList)
+                    {
+                        decFace = new DetectedFaceExtended
+                        {
+                            FaceRectangle = face.FaceRectangle,
+
+                        };
+                        decFace.PredominantEmotion = FindDetectedEmotion(face.FaceAttributes.Emotion);
+                        detectedFaces.Add(decFace);
+                    }
                 }
-                
+   
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
+                HideProgressDialog();
+                UserDialogs.Instance.Toast("Could not detect any face");
                 
             }
-            return faces;
+            return facesFound;
         }
 
 
@@ -140,37 +151,29 @@ namespace XF.Azure.CS.FaceAPI.View
 
         private void OnClickTapped(object sender, EventArgs e)
         {
-            
+
             TakePictureAndAnalizeImage();
-            
-        }
-        private void OnDetailsTapped(object sender, EventArgs e)
-        {
-            UserDialogs.Instance.Alert(lastResult, "Raw", "Ok");
+
         }
         
 
-        private void ResetResultLabel()
-        {
-            result.Text = "Detected emotion: ";
-        }
+
+        
 
         private async void TakePictureAndAnalizeImage()
         {
-            ResetResultLabel();
+            
             var capturedImg = await TakePicture();
             if (capturedImg != null)
             {
                 ShowProgressDialog();
                 SetImageInImageView(capturedImg);
-                var emotions = await GetFaces(capturedImg);
-                if(emotions.Count>0)
+                var facesFound = await GetFaces(capturedImg);
+                if (facesFound)
                 {
                     capturedImage.InvalidateSurface();
-                    var detectedEmotion = FindDetectedEmotion(emotions);
-                    result.Text += detectedEmotion;
                 }
-                
+
                 HideProgressDialog();
             }
         }
@@ -190,10 +193,10 @@ namespace XF.Azure.CS.FaceAPI.View
         {
             var info = args.Info;
             var canvas = args.Surface.Canvas;
-            ClearCanvas(info,canvas);
+            ClearCanvas(info, canvas);
             if (Image != null)
             {
-                var scale = Math.Min((float)info.Width / (float)Image.Width, (float)info.Height / (float)Image.Height);
+                var scale = Math.Min(info.Width / (float)Image.Width, info.Height / (float)Image.Height);
 
                 var scaleHeight = scale * Image.Height;
                 var scaleWidth = scale * Image.Width;
@@ -203,30 +206,70 @@ namespace XF.Azure.CS.FaceAPI.View
 
                 canvas.DrawBitmap(Image, new SKRect(left, top, left + scaleWidth, top + scaleHeight));
 
-                if(faceRectangles.Count>0)
+                if (detectedFaces.Count > 0)
                 {
-                    foreach(var face in faceRectangles)
+                    foreach (var face in detectedFaces)
                     {
-                        LabelPrediction(canvas, face, left, top , scale);
+                        LabelPrediction(canvas, face.FaceRectangle, left, top, scale , face.PredominantEmotion);
+
                     }
-                    
+
                 }
             }
         }
 
-       
 
-        static void LabelPrediction(SKCanvas canvas, FaceRectangle box, float left, float top, float scale, bool addBox = true)
+
+        static void LabelPrediction(SKCanvas canvas, FaceRectangle box, float left, float top, float scale, string emotion)
         {
-            var scaledBoxLeft = left + (scale * (float)box.Left);
-            var scaledBoxWidth = scale * (float)box.Width;
-            var scaledBoxTop = top + (scale * (float)box.Top);
-            var scaledBoxHeight = scale * (float)box.Height;
+            var scaledBoxLeft = left + (scale * box.Left);
+            var scaledBoxWidth = scale * box.Width;
+            var scaledBoxTop = top + (scale * box.Top);
+            var scaledBoxHeight = scale * box.Height;
 
-            if (addBox)
-                DrawBox(canvas, scaledBoxLeft, scaledBoxTop, scaledBoxWidth, scaledBoxHeight);
 
-            
+            DrawBox(canvas, scaledBoxLeft, scaledBoxTop, scaledBoxWidth, scaledBoxHeight);
+            DrawText(canvas, emotion, scaledBoxLeft, scaledBoxTop, scaledBoxWidth, scaledBoxHeight);
+
+        }
+
+        static void DrawText(SKCanvas canvas, string tag, float startLeft, float startTop, float scaledBoxWidth, float scaledBoxHeight)
+        {
+            var textPaint = new SKPaint
+            {
+                IsAntialias = true,
+                Color = SKColors.White,
+                Style = SKPaintStyle.Fill,
+                Typeface = SKTypeface.FromFamilyName("Arial")
+            };
+
+            var text = tag;
+
+            var textWidth = textPaint.MeasureText(text);
+            textPaint.TextSize = 0.9f * scaledBoxWidth * textPaint.TextSize / textWidth;
+
+            var textBounds = new SKRect();
+            textPaint.MeasureText(text, ref textBounds);
+
+            var xText = (startLeft + (scaledBoxWidth / 2)) - textBounds.MidX;
+            var yText = (startTop + (scaledBoxHeight / 2)) + textBounds.MidY;
+
+            var paint = new SKPaint
+            {
+                Style = SKPaintStyle.Fill,
+                Color = new SKColor(0, 0, 0, 120)
+            };
+
+            var backgroundRect = textBounds;
+            backgroundRect.Offset(xText, yText);
+            backgroundRect.Inflate(10, 10);
+
+            canvas.DrawRoundRect(backgroundRect, 5, 5, paint);
+
+            canvas.DrawText(text,
+                            xText,
+                            yText,
+                            textPaint);
         }
 
         static void DrawBox(SKCanvas canvas, SKPaint paint, float startLeft, float startTop, float scaledBoxWidth, float scaledBoxHeight)
