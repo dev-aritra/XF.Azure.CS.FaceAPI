2432560df7a9cc256cb97710154afa9ff320704e
dev-aritra <dev.aritradas@gmail.com>
2018-11-14 12:45:55 +0530

    added splashscreen for android

--- XF.Azure.CS.FaceAPI/XF.Azure.CS.FaceAPI.Android/MainActivity.cs
+++ XF.Azure.CS.FaceAPI/XF.Azure.CS.FaceAPI.Android/MainActivity.cs
@@ -11,7 +11,7 @@ using Acr.UserDialogs;
 
 namespace XF.Azure.CS.FaceAPI.Droid
 {
-    [Activity(Label = "XF.Azure.CS.FaceAPI", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Theme = "@style/MainTheme")]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
